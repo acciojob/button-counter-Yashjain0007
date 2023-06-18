7bdbaa4d7a949b9777eadf259d8a2f2dfaed793c
Yash Jain <124593014+Yashjain0007@users.noreply.github.com>
2023-06-18 06:58:39 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let[click,setClick] = useState("");
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {click} times</p>
+        <button onClick={setClick(click+1)}>Click me</button>
+
     </div>
   )
 }
